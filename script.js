a60ecfb2f385c56830008ad757e93cd28be37159
cautcepa <162703540+cautcepa@users.noreply.github.com>
2024-04-17 13:46:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+document.getElementById('drawButton').addEventListener('click', function() {
+  fetch('cadastrados.json')
+    .then(response => response.json())
+    .then(data => {
+      const names = data.names;
+      const index = Math.floor(Math.random() * names.length);
+      document.getElementById('winnerName').textContent = names[index];
+      // Confirma se há nome para ativar confetes
+      if (names[index]) {
+        window.confetti({
+          particleCount: 150,
+          spread: 70,
+          origin: { y: 0.6 }
+        });
+      }
+    })
+    .catch(error => {
+      console.error('Erro ao buscar os nomes:', error);
+      document.getElementById('winnerName').textContent = 'Erro ao carregar nomes!';
+    });
+});
