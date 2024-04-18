badc9df81387ef0c2a7720f4e27d1d2bfa6f5993
cautcepa <162703540+cautcepa@users.noreply.github.com>
2024-04-18 09:57:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,10 +8,12 @@ document.getElementById('drawButton').addEventListener('click', function() {
       // Verifica se há um nome para ativar os confetes
       if (names[index]) {
         window.confetti({
-          particleCount: 1000,
-          spread: 70,
-          angle: 90,           // Define o ângulo para que os confetes sejam lançados para baixo
-          origin: { y: 0 }     // Configura a origem no topo do viewport
+          particleCount: 200, // Aumenta o número de confetes para uma chuva mais intensa
+          spread: 360,        // Define o spread como 360 para que os confetes cubram toda a tela horizontalmente
+          angle: 90,          // Define o ângulo para que os confetes sejam lançados para baixo
+          origin: { y: 0 },   // Configura a origem no topo do viewport
+          colors: ['#FF0000', '#00FF00', '#0000FF'], // Cores mais fortes e vívidas
+          alpha: 1            // Ajusta a transparência para 1 (opaco)
         });
       }
     })
