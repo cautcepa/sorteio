07c399792408c675c4ead08260ddd482e7750b5f
cautcepa <162703540+cautcepa@users.noreply.github.com>
2024-04-18 09:51:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
 document.getElementById('drawButton').addEventListener('click', function() {
-  console.log('Botão clicado'); // Isso deve aparecer no console quando o botão for clicado.
   fetch('cadastrados.json')
     .then(response => response.json())
     .then(data => {
       const names = data.names;
       const index = Math.floor(Math.random() * names.length);
       document.getElementById('winnerName').textContent = names[index];
+      // Verifica se há um nome para ativar os confetes
       if (names[index]) {
-        // Dispara confetes de várias origens ao longo do eixo x
-        for (let i = 0; i < 10; i++) { // 10 lançamentos de confetes ao longo do topo
-          window.confetti({
-            particleCount: 20, // menor contagem para cada lançamento individual
-            angle: 90,
-            spread: 55, // Spread mais estreito para cada lançamento
-            origin: { x: i * 0.1, y: 0 }, // Varia a posição x de 0 a 1
-            gravity: 0.4,
-            drift: 0.4
-          });
-        }
+        window.confetti({
+          particleCount: 150,
+          spread: 70,
+          angle: 90,           // Define o ângulo para que os confetes sejam lançados para baixo
+          origin: { y: 0 }     // Configura a origem no topo do viewport
+        });
       }
     })
     .catch(error => {
